f2710d8e46ead658f66fd3a1ef268018321f3ed7
Faik Turan <turanfaik@hotmail.com>
2021-11-22 19:51:04 +0300

    Add project files.

---
+++ MCD_ArrayListOdev2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD_ArrayListOdev2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
